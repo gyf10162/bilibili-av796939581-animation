15ef835aaf688052e784f16d03c44650abd22d7b
gyf10162 <49122388+gyf10162@users.noreply.github.com>
2020-09-16 11:51:10 +0800

    调整编码和格式

--- show_animation_all.m
+++ show_animation_all.m
@@ -1,18 +1,18 @@
-% ä»CSVæ–‡ä»¶è·å–æ­¥éª¤ï¼Œè¿›è¡Œæ‰¹é‡æ¼”ç¤º
+% ´ÓCSVÎÄ¼ş»ñÈ¡²½Öè£¬½øĞĞÅúÁ¿ÑİÊ¾
 % Author GYF
 % bilibili @gyf10162
 close all;
 clear;
-hFig = figure('Position', [1920+100, 100, 700, 700]);
+hFig = figure('Position', [1920 + 100, 100, 700, 700]);
 filename = 'result.csv';
 [xs, ys, canSolve, Steps] = textread(filename, '%d%d%d%s', 'delimiter', ',');
-% videoObj = VideoWriter('output.mp4','MPEG-4');
+% videoObj = VideoWriter('output.mp4', 'MPEG-4');
 % fps = 60;
 % videoObj.FrameRate = fps;
+% videoObj.Quality = 95;
 % videoObj.open();
 for i = 1:length(xs)
     show_animation([xs(i), ys(i)], Steps{i}, canSolve(i));
-    %     show_animation([xs(i),ys(i)],Steps{i},canSolve(i)),videoObj,fps);
+    % show_animation([xs(i), ys(i)], Steps{i}, canSolve(i), videoObj, fps);
 end
-
 % videoObj.close();
